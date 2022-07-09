2d99eea1f9c2e5990fdb380c2b9e990f4d2a875a
saeedalipoorcom <saeedalipoor2022@gmail.com>
2022-07-09 19:52:52 +0430

    Tested

--- contracts/Master.sol
+++ contracts/Master.sol
@@ -14,7 +14,7 @@ import "./NFTContract.sol";
 
 contract Master is Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter public IDTracker;
+    Counters.Counter public auctionIDTracker;
 
     DescriptorStorage DescriptorStorageContract;
     NFTContract NFTToken;
@@ -27,7 +27,20 @@ contract Master is Ownable {
         uint48 glasses;
     }
 
+    struct Auction {
+        uint256 auctionID;
+        uint256 amount;
+        uint256 startTime;
+        uint256 endTime;
+        address payable bidder;
+        bool finished;
+    }
+    Auction public auction;
+
     string public LastCreatedSVG;
+    uint256 public duration = 10;
+
+    bool started;
 
     // // /////////////////////////////////////////////////////////////// SET ADDRESS
 
@@ -52,10 +65,9 @@ contract Master is Ownable {
     // // /////////////////////////////////////////////////////////////// SEED FUNCTIONS
 
     function generateSeed(uint256 nounId)
-        external
+        internal
         view
-        onlyOwner
-        returns (Seed memory)
+        returns (INounsSeeder.Seed memory)
     {
         uint256 pseudorandomness = uint256(
             keccak256(abi.encodePacked(blockhash(block.number - 1), nounId))
@@ -67,23 +79,21 @@ contract Master is Ownable {
         uint256 headCount = DescriptorStorageContract.headCount();
         uint256 glassesCount = DescriptorStorageContract.glassesCount();
 
-        return
-            Seed({
-                background: uint48(uint48(pseudorandomness) % backgroundCount),
-                body: uint48(uint48(pseudorandomness >> 48) % bodyCount),
-                accessory: uint48(
-                    uint48(pseudorandomness >> 96) % accessoryCount
-                ),
-                head: uint48(uint48(pseudorandomness >> 144) % headCount),
-                glasses: uint48(uint48(pseudorandomness >> 192) % glassesCount)
-            });
+        INounsSeeder.Seed memory _newSeed = INounsSeeder.Seed({
+            background: uint48(uint48(pseudorandomness) % backgroundCount),
+            body: uint48(uint48(pseudorandomness >> 48) % bodyCount),
+            accessory: uint48(uint48(pseudorandomness >> 96) % accessoryCount),
+            head: uint48(uint48(pseudorandomness >> 144) % headCount),
+            glasses: uint48(uint48(pseudorandomness >> 192) % glassesCount)
+        });
+
+        return _newSeed;
     }
 
     // // /////////////////////////////////////////////////////////////// SVG IMAGE FUNCTIONS
 
     function generateSVGImage(INounsSeeder.Seed memory seed)
-        external
-        onlyOwner
+        internal
         returns (string memory)
     {
         LastCreatedSVG = DescriptorStorageContract.generateSVGImage(seed);
@@ -93,5 +103,90 @@ contract Master is Ownable {
     // // /////////////////////////////////////////////////////////////// MINT NEW NFT FOR SOLD SVG
     function mintNewNFT(address _mintTo) external onlyOwner {
         NFTToken.mintNewNFT(LastCreatedSVG, _mintTo);
+    }
+
+    // // /////////////////////////////////////////////////////////////// AUCTION
+
+    function finishCurrentAndCreateNewAuction() external {
+        _finishCurrentAuction();
+        _createAuction();
+    }
+
+    function startGame() external {
+        require(started == false, "Already started !");
+        _createAuction();
+        started = true;
+    }
+
+    function _createAuction() internal {
+        auctionIDTracker.increment();
+        uint256 _auctionIDTracker = auctionIDTracker.current();
+
+        uint256 startTime = block.timestamp;
+        uint256 endTime = startTime + duration;
+
+        INounsSeeder.Seed memory _newSeed = generateSeed(_auctionIDTracker);
+        generateSVGImage(_newSeed);
+
+        auction = Auction({
+            auctionID: _auctionIDTracker,
+            amount: 0,
+            startTime: startTime,
+            endTime: endTime,
+            bidder: payable(0),
+            finished: false
+        });
+    }
+
+    function finishCurrentAuction() external {
+        _finishCurrentAuction();
+    }
+
+    function _finishCurrentAuction() internal {
+        Auction memory _auction = auction;
+
+        require(_auction.startTime != 0, "Auction hasn't begun");
+        require(!_auction.finished, "Auction has already been settled");
+        require(
+            block.timestamp >= _auction.endTime,
+            "Auction hasn't completed"
+        );
+
+        auction.finished = true;
+
+        if (_auction.bidder != address(0)) {
+            NFTToken.mintNewNFT(LastCreatedSVG, _auction.bidder);
+        }
+    }
+
+    // // /////////////////////////////////////////////////////////////// BID ON AUCTION
+
+    function createBid(uint256 _auctionIDTracker) external payable {
+        Auction memory _auction = auction;
+
+        require(
+            _auction.auctionID == _auctionIDTracker,
+            "Noun not up for auction"
+        );
+        require(block.timestamp < _auction.endTime, "Auction expired");
+        require(
+            msg.value >= _auction.amount,
+            "Must send more than last bid by minBidIncrementPercentage amount"
+        );
+
+        address payable lastBidder = _auction.bidder;
+
+        // Refund the last bidder, if applicable
+        if (lastBidder != address(0)) {
+            lastBidder.transfer(_auction.amount);
+        }
+
+        auction.amount = msg.value;
+        auction.bidder = payable(msg.sender);
+    }
+
+    // // /////////////////////////////////////////////////////////////// GET CURRENT AUCTION
+    function getCurrentAuctionDetails() external view returns (Auction memory) {
+        return auction;
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -4,23 +4,34 @@ const { expect } = require("chai");
 const ImageData = require("./utils/imageData.json");
 
 describe("Test Deploy", async function () {
-  it("Test Pro", async function () {
-    const [, addr1] = await ethers.getSigners();
+  let NFTContract;
+  let MasterContract;
+  let DescriptorStorageContract;
+
+  let TX;
+  let currentAuction;
+
+  let addr1;
+  let addr2;
+  let addr3;
+
+  beforeEach(async function () {
+    [, addr1, addr2, addr3] = await ethers.getSigners();
 
     // DEPLOYMENT NFTContract
     const NFT = await ethers.getContractFactory("NFTContract");
-    const NFTContract = await NFT.deploy();
+    NFTContract = await NFT.deploy();
     await NFTContract.deployed();
 
     // DEPLOYMENT MY CONTRACTS
     const Master = await ethers.getContractFactory("Master");
-    const MasterContract = await Master.deploy();
+    MasterContract = await Master.deploy();
     await MasterContract.deployed();
 
     const DescriptorStorage = await ethers.getContractFactory(
       "DescriptorStorage"
     );
-    const DescriptorStorageContract = await DescriptorStorage.deploy();
+    DescriptorStorageContract = await DescriptorStorage.deploy();
     await DescriptorStorageContract.deployed();
 
     await MasterContract.setNounsDescriptor(DescriptorStorageContract.address);
@@ -63,23 +74,144 @@ describe("Test Deploy", async function () {
 
     await NFTContract.transferOwnership(MasterContract.address);
     await DescriptorStorageContract.transferOwnership(MasterContract.address);
+  });
 
-    // TEST DATA
-    const nounId = 1;
+  describe("Start Project Once !", () => {
+    it("", async () => {
+      TX = await MasterContract.startGame();
+      await TX.wait();
 
-    const seed = await MasterContract.generateSeed(nounId);
-    console.log(seed.toString());
+      const currentAuction = await MasterContract.getCurrentAuctionDetails();
 
-    const svg = await MasterContract.generateSVGImage(seed);
-    await svg.wait(1);
+      // check currentAuction auction to be equal 1
+      expect(currentAuction[0].toString()).to.equal("1");
+      // check currentAuction last amount to be equal 0
+      expect(currentAuction[1].toString()).to.equal("0");
+      // check currentAuction bidder to be equal address 0
+      expect(currentAuction[4].toString()).to.equal(
+        "0x0000000000000000000000000000000000000000"
+      );
+      // check currentAuction is finished to be equal false
+      expect(currentAuction[5].toString()).to.equal("false");
 
-    const createdSVG = await MasterContract.LastCreatedSVG();
-    console.log(createdSVG.toString());
+      // and we will get revert because we can't again use  startGame function to start any auction
+      await expect(MasterContract.startGame()).to.be.revertedWith(
+        "Already started !"
+      );
+    });
+  });
 
-    const newNFTID = await MasterContract.mintNewNFT(addr1.address);
-    await newNFTID.wait(1);
+  describe("Bid On Current Auction , check revert options ", () => {
+    beforeEach(async function () {
+      TX = await MasterContract.startGame();
+      await TX.wait();
+    });
+    it("", async () => {
+      const currentAuctionID = 1;
 
-    expect(await NFTContract.tokenURI(1)).to.equal(createdSVG.toString());
-    expect(await NFTContract.ownerOf(1)).to.equal(addr1.address);
+      // make bid on current running auction
+      TX = await MasterContract.connect(addr1).createBid(currentAuctionID, {
+        value: ethers.utils.parseEther("1"),
+      });
+      await TX.wait(1);
+
+      // now check auction details with ID #1
+      currentAuction = await MasterContract.getCurrentAuctionDetails();
+
+      // check currentAuction auction to be equal 1
+      expect(currentAuction[0].toString()).to.equal("1");
+      // check currentAuction last amount to be equal 1 ether
+      expect(currentAuction[1]).to.equal(ethers.utils.parseEther("1"));
+      // check currentAuction bidder to be equal address addre1
+      expect(currentAuction[4]).to.equal(addr1.address);
+      // check currentAuction is finished to be equal false
+      expect(currentAuction[5].toString()).to.equal("false");
+
+      // now check if we get revert !
+      await expect(
+        MasterContract.connect(addr2).createBid(currentAuctionID, {
+          value: ethers.utils.parseEther("0.5"),
+        })
+      ).to.be.revertedWith(
+        "Must send more than last bid by minBidIncrementPercentage amount"
+      );
+
+      await expect(
+        MasterContract.connect(addr2).createBid(2, {
+          value: ethers.utils.parseEther("1"),
+        })
+      ).to.be.revertedWith("Noun not up for auction");
+
+      // addr3 make more bid !
+      TX = await MasterContract.connect(addr3).createBid(currentAuctionID, {
+        value: ethers.utils.parseEther("2"),
+      });
+      await TX.wait(1);
+
+      // now check auction details with ID #1
+      currentAuction = await MasterContract.getCurrentAuctionDetails();
+
+      // check currentAuction auction to be equal 1
+      expect(currentAuction[0].toString()).to.equal("1");
+      // check currentAuction last amount to be equal 1 ether
+      expect(currentAuction[1]).to.equal(ethers.utils.parseEther("2"));
+      // check currentAuction bidder to be equal address addre1
+      expect(currentAuction[4]).to.equal(addr3.address);
+      // check currentAuction is finished to be equal false
+      expect(currentAuction[5].toString()).to.equal("false");
+
+      // now mine new blocks to get auction end time ! and current auction will get expire !
+      for (let i = 0; i < 10; i++) {
+        ethers.provider.send("evm_mine");
+      }
+
+      await expect(
+        MasterContract.connect(addr2).createBid(1, {
+          value: ethers.utils.parseEther("1"),
+        })
+      ).to.be.revertedWith("Auction expired");
+    });
+  });
+
+  describe("Bid On Current Auction , Mint NFT For Winner, Start New Auction", () => {
+    beforeEach(async function () {
+      TX = await MasterContract.startGame();
+      await TX.wait();
+    });
+    it("", async () => {
+      const currentAuctionID = 1;
+
+      // make bid on current running auction
+      TX = await MasterContract.connect(addr1).createBid(currentAuctionID, {
+        value: ethers.utils.parseEther("1"),
+      });
+      await TX.wait(1);
+
+      // now mine new blocks to get auction end time ! and current auction will get expire !
+      for (let i = 0; i < 10; i++) {
+        ethers.provider.send("evm_mine");
+      }
+
+      // finish auction
+      TX = await MasterContract.finishCurrentAndCreateNewAuction();
+      await TX.wait(1);
+
+      // now check auction details with ID #2 because master contract created new auction
+      currentAuction = await MasterContract.getCurrentAuctionDetails();
+
+      // check currentAuction auction to be equal 1
+      expect(currentAuction[0].toString()).to.equal("2");
+      // check currentAuction last amount to be equal 1 ether
+      expect(currentAuction[1]).to.equal(ethers.utils.parseEther("0"));
+      // check currentAuction bidder to be equal address addre1
+      expect(currentAuction[4]).to.equal(
+        "0x0000000000000000000000000000000000000000"
+      );
+      // check currentAuction is finished to be equal false
+      expect(currentAuction[5].toString()).to.equal("false");
+
+      // we minted new NFT with ID 1 for winner of auction with ID 1 ! let's check it
+      expect(await NFTContract.ownerOf(1)).to.equal(addr1.address);
+    });
   });
 });
